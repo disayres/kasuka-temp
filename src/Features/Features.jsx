e18e038d80cfd51ae70cfc9f193ed301640fae6b
Ali <alipapi5426771@gmail.com>
2025-02-23 08:43:43 +0330

    fix : change props usage methods

--- src/Features/Features.jsx
+++ src/Features/Features.jsx
@@ -1,6 +1,12 @@
 import FeatureItems from "../FeatureItems/FeatureItems"
 
 export default function Features() {
+    const allFeatures = [
+        { id: 1, title: "تبلیغ سختی است", text: "نتایج یا در نتیجه هر یک از این اهداف اشتغال حاصل می شود" },
+        { id: 2, title: "کدام یک از اینهاست", text: "کسانی که هوس سیاهان می کنند نمی بینند ، آنها مقصر هستند که خدمات خود را رها کرده اند" },
+        { id: 3, title: "یا کور شده است", text: "یا می پذیرد یا زمانی که هیچ کس از همه اجتناب نمی کند. همه دردهایی که آنها را بیشتر می کند" },
+        { id: 4, title: "حقیقت مبارک", text: "آنها بدون تحمل حقیقت در زمان ستایش زندگی از هیچ چیزی پیروی نمی کنند" },
+    ]
     return (
         <>
             <section id="features">
@@ -10,10 +16,10 @@ export default function Features() {
                             <img src="./images/features.jpg" alt="features-bg" className="max-w-[660px] w-full" />
                         </div>
                         <div className="space-y-8">
-                            <FeatureItems title="تبلیغ سختی است" text="نتایج یا در نتیجه هر یک از این اهداف اشتغال حاصل می شو" />
-                            <FeatureItems title="کدام یک از اینهاست" text="کسانی که هوس سیاهان می کنند نمی بینند ، آنها مقصر هستند که خدمات خود را رها کرده اند" />
-                            <FeatureItems title="یا کور شده است" text="یا می پذیرد یا زمانی که هیچ کس از همه اجتناب نمی کند. همه دردهایی که آنها را بیشتر می کند" />
-                            <FeatureItems title="حقیقت مبارک" text="آنها بدون تحمل حقیقت در زمان ستایش زندگی از هیچ چیزی پیروی نمی کنند" />
+                            <FeatureItems {...allFeatures[0]} />
+                            <FeatureItems {...allFeatures[1]} />
+                            <FeatureItems {...allFeatures[2]} />
+                            <FeatureItems {...allFeatures[3]} />
                         </div>
                     </div>
                 </div>
--- src/HeroSection/HeroSection.jsx
+++ src/HeroSection/HeroSection.jsx
@@ -1,6 +1,13 @@
 import HeroCard from "../HeroCard/HeroCard"
 
 export default function HeroSection() {
+    let CardsInformation = [
+        { id: 1, title: "متن نمایشی", imgUrl: "./images/svgs/chart.svg" },
+        { id: 2, title: "نمودار کیفی", imgUrl: "./images/svgs/chart.svg" },
+        { id: 3, title: "نمونه کار ها", imgUrl: "./images/svgs/chart.svg" },
+        { id: 4, title: "عملکرد های پایانی", imgUrl: "./images/svgs/performance.svg" },
+        { id: 4, title: "اطلاعات ذخیره شده", imgUrl: "./images/svgs/database.svg" },
+    ]
     return (
         <section id="hero" className="main-hero relative w-full h-screen bg-fixed bg-cover bg-no-repeat bg-[bottom,center] text-white">
             <div className="container">
@@ -13,12 +20,12 @@ export default function HeroSection() {
                         </h1>
                         <p className="mt-2.5 text-xl xs:text-2xl">ما تیمی از بازاریابان با استعداد دیجیتال هستیم</p>
                     </div>
-                    <div className="flex flex-wrap justify-center gap-6 mt-12">
-                        <HeroCard />
-                        <HeroCard />
-                        <HeroCard />
-                        <HeroCard />
-                        <HeroCard />
+                    <div className="grid grid-cols-5 justify-center gap-6 mt-12">
+                        <HeroCard {...CardsInformation[0]} />
+                        <HeroCard {...CardsInformation[1]} />
+                        <HeroCard {...CardsInformation[2]} />
+                        <HeroCard {...CardsInformation[3]} />
+                        <HeroCard {...CardsInformation[4]} />
                     </div>
                 </div>
             </div>
